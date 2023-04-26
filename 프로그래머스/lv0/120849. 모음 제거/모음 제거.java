247a62af1daa48baeefb4f2ac4b6a3b6cb7fa2fe
Lee-KyuHwun <107871734+Lee-Kyuhwun@users.noreply.github.com>
2023-04-27 07:03:04 +0900

    [level 0] Title: 모음 제거, Time: 9.35 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String my_string) {
+        String answer = "";
+        for(int i=0; i<my_string.length(); i++){
+            if(my_string.charAt(i) == 'i'|| my_string.charAt(i) == 'o'||  my_string.charAt(i)  == 'e'||   my_string.charAt(i) == 'u'|| my_string.charAt(i) == 'a'){ 
+                continue;}
+                
+                answer = answer + my_string.charAt(i);
+        }
+        return answer;
+    }
+}
