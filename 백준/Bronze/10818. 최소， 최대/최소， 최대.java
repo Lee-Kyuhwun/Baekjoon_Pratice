dd9d86ac9e7f679a45c3fc0e61a1f142ccf0b0e3
Lee KyuHwun <107871734+Lee-Kyuhwun@users.noreply.github.com>
2024-02-12 03:50:03 +0900

    [Bronze III] Title: 최소, 최대, Time: 1680 ms, Memory: 318092 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,19 +1,34 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[]args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
 
-        int[] a= new int[n];
-        for(int i =0 ;i<n ; i++){
+        if (n <= 0) {
+            scanner.close();
+            return; // 배열의 크기가 0 이하인 경우 처리를 종료
+        }
+
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
             a[i] = scanner.nextInt();
         }
-        System.out.printf("%d "+"%d", Arrays.stream(a).min().getAsInt(), Arrays.stream(a).max().getAsInt());
 
+        // 배열 a를 순회하면서 최소값과 최대값을 찾음
+        int min = a[0];
+        int max = a[0];
+        for (int i = 1; i < n; i++) {
+            if (a[i] < min) {
+                min = a[i];
+            }
+            if (a[i] > max) {
+                max = a[i];
+            }
+        }
+
+        System.out.printf("%d %d", min, max);
 
         scanner.close();
-
     }
 }
